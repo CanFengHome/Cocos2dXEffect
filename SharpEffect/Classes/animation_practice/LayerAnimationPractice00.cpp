098c2a711556943f250254d66c962a4b142051ab
CanFengHoem <csdnfor@126.com>
2016-03-24 09:13:59 +0800

    泡泡动画实践

--- SharpEffect/Classes/animation_practice/LayerAnimationPractice00.cpp
+++ SharpEffect/Classes/animation_practice/LayerAnimationPractice00.cpp
@@ -11,17 +11,26 @@
 USING_NS_CC;
 bool LayerAnimationPractice00::init()
 {
-    if (!LayerColor::initWithColor(Color4B::WHITE))
+    if (!LayerColor::initWithColor(Color4B::GRAY))
     {
         return false;
     }
     
     Size winSize = Director::getInstance()->getWinSize();
     
-    Sprite* pRoleSprite = Sprite::create("res/role/role1.png");
-    pRoleSprite->setPosition3D(Vec3(winSize.width*0.5f,winSize.height*0.5f,0.0f));
-    pRoleSprite->runAction(MoveTo::create(2.0f, Vec3(winSize.width*0.5f, winSize.height*0.5f, -500.0f)));
+    Sprite* pRoleSprite = Sprite::create("res/ui/bubble.png");
+    pRoleSprite->setPosition(winSize.width*0.5f,winSize.height*0.5f);
     addChild(pRoleSprite);
+    
+    float dt = 0.5f;
+    float contentScale = 1.0f;
+    pRoleSprite->setScale(contentScale);
+    // 对EaseSine曲线的描述 http://blog.sina.com.cn/s/blog_61e26bcb0101924u.html
+    ActionInterval* pScaleAction = Sequence::create(
+          EaseSineOut::create(ScaleTo::create(dt * 0.5f, 1.05f * contentScale, 0.95f * contentScale)),
+          EaseSineInOut::create(ScaleTo::create(dt, 0.95f* contentScale, 1.05f * contentScale)),
+          EaseSineIn::create(ScaleTo::create(dt * 0.5f, contentScale, contentScale)), nullptr);
+    pRoleSprite->runAction(RepeatForever::create(pScaleAction));
     
     return true;
 }
